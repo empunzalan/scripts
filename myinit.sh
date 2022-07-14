39ee4c913af9729f6b9e3667b0032c5f6e3ee4cb
edgar <edgar.punzalan@truefort.com>
2022-07-15 00:19:42 +0800

    new files

---
+++ myinit.sh
@@ -0,0 +1,3 @@
+#!/bin/bas
+
+useradd -m samurai
---
+++ spawndir.ps1
@@ -0,0 +1,9 @@
+# Edgar Punzalan - 07042022
+# Generates directories and empty files based on parameters passed
+
+# FILESPERDIR  MIN 0
+# DEPTH        MIN 1
+# SPATH        DEFAULT PWD
+
+Set-Variable -Name "filesperdir" -Value 2
+Set-Variable -Name "depth" -Value 2
